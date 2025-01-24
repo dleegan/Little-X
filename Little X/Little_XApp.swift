fc0ebf43ab39ba141a81c1d4f5178a0be6aebb94
DUPROS Leegan <leegan.dupros@epitech.eu>
2025-01-24 22:07:20 +0100

    Initial Commit

---
+++ Little X/Little_XApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_XApp.swift
+//  Little X
+//
+//  Created by dleegan on 24/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Little_XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
